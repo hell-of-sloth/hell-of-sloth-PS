b807925074af35ec1b0740ba69d25b88d4b83601
caboodles <sanghyup2000@gmail.com>
2023-07-06 11:12:15 +0900

    get next pal 9로만 이루어진 경우 구현

--- sanghyup/1747_2.py
+++ sanghyup/1747_2.py
@@ -12,27 +12,40 @@ def isPrimeFunc(num =int): # 소수인지 판별해주는 함수
 def makePalindrome(num=int): # 팰린드롬 수를 만들어주는 함수
     num = str(num)
     strlenght = len(num)
-    num = num[0:math.ceil(strlenght/2)] # 입력값의 절반 홀수일 경우 중간 값 포함 #num은 string임
-    palindrome = num
+    half = num[0:math.ceil(strlenght/2)] # 입력값의 절반 홀수일 경우 중간 값 포함 #num은 string임
+    palindrome = half
     if strlenght % 2: # 홀수일 경우
-        palindrome += num[-2::-1]
+        palindrome += half[-2::-1]
     else: # 짝수일 경우
-        palindrome += num[::-1]
+        palindrome += half[::-1]
     return int(palindrome)
 
-def getNextPalindrome(num=int, temp):
+def getNextPalindrome(num=int):
     num = str(num)
     strlenght = len(num)
-    num = num[0:math.ceil(strlenght/2)] # 입력값의 절반 홀수일 경우 중간 값 포함 #num은 string임
+    half = num[0:math.ceil(strlenght/2)] # 입력값의 절반 홀수일 경우 중간 값 포함 #num은 string임
     is_nines = True
-    for i in num:
+    for i in num: # 9로만 이루어져 있는지 확인
         if i != '9':
             is_nines = False
             break
-    
+    palindrome = ''
+    if is_nines == True:
+        palindrome = '1'
+        for i in range(strlenght-1):
+            palindrome += '0'
+        palindrome += '1'
+    if is_nines == False:
+        if strlenght % 2: # 홀수일 경우
+            half = str(int(half)+1)
+            palindrome = half
+            palindrome += half[-2::-1]
+
+    return int(palindrome)
 
 
 N = int(sys.stdin.readline())
 print(makePalindrome(N))
+print(getNextPalindrome(N))
 
 
