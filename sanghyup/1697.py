6f701a3f22be62e0c79178d2bfa0d6f08f0c528b
caboodles <sanghyup2000@gmail.com>
2023-07-09 23:36:56 +0900

    7/9 업보 청산

--- sanghyup/1697.py
+++ sanghyup/1697.py
@@ -1,6 +1,18 @@
 import sys
-
+from collections import deque
 
 input = list(map(int,sys.stdin.readline().split()))
 
-print(input)
+timeTaken = [0 for i in range(100001)]
+
+bfs = deque()
+bfs.append(input[0])
+while True:
+    now = bfs.popleft()
+    if now == input[1]:
+        print(timeTaken[now])
+        break
+    for next in [now-1,now+1,now*2]:
+        if 0<=next and next<=100000 and timeTaken[next] == 0:
+            timeTaken[next] = timeTaken[now] + 1
+            bfs.append(next)
