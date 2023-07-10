2c5692ab7b2fb13ceb5a2dcb59aacd902595658b
caboodles <sanghyup2000@gmail.com>
2023-07-10 15:34:20 +0900

    1차 시도

--- sanghyup/3085.py
+++ sanghyup/3085.py
@@ -17,26 +17,57 @@ for i in range(n):
         combo = True
         changed = False
         longestCombo = 1
+        if _-1 >=0:
+                if i-1>=0 and board[i-1][_-1]==board[i][_]:
+                    longestCombo+=1
+                    changed = True
+                elif i+1<n and board[i+1][_-1]==board[i][_]:
+                    longestCombo+=1
+                    changed = True
         for j in range(_, n-1):
-            if j-1 >=0:
-                if i-1>=0 and board[i-1][j-1]==board[i][j]:
-                    longestCombo+=1
-                    changed == True
-                elif i+1<n and board[i+1][j-1]==board[i][j]:
-                    longestCombo+=1
-                    changed == True
             if j+1< n:
-                if board[i][j+1]==board[i][j]:
+                if board[i][j+1]==board[i][_]:
                     longestCombo+=1
-                elif changed == False and i-1>=0 and board[i-1][j+1]==board[i][j]:
+                elif changed == False and i-1>=0 and board[i-1][j+1]==board[i][_]:
                     longestCombo+=1
-                    changed == True
-                elif changed == False and i+1<n and board[i+1][j+1]==board[i][j]:
+                    changed = True
+                elif changed == False and i+1<n and board[i+1][j+1]==board[i][_]:
                     longestCombo+=1
-                    changed == True
+                    changed = True
                 else:
                     break
-            print(i,_, j, board[i][j], longestCombo)
+        print(i,_,board[i][_] ,longestCombo)
+        if longestCombo > answer:
+            answer = longestCombo
+
+board = invertMatrix(board)
+
+for i in range(n):
+    for _ in range(n):
+        combo = True
+        changed = False
+        longestCombo = 1
+        if _-1 >=0:
+                if i-1>=0 and board[i-1][_-1]==board[i][_]:
+                    longestCombo+=1
+                    changed = True
+                elif i+1<n and board[i+1][_-1]==board[i][_]:
+                    longestCombo+=1
+                    changed = True
+        for j in range(_, n-1):
+            if j+1< n:
+                if board[i][j+1]==board[i][_]:
+                    longestCombo+=1
+                elif changed == False and i-1>=0 and board[i-1][j+1]==board[i][_]:
+                    longestCombo+=1
+                    changed = True
+                elif changed == False and i+1<n and board[i+1][j+1]==board[i][_]:
+                    longestCombo+=1
+                    changed = True
+                else:
+                    break
+        print(i,_,board[i][_] ,longestCombo)
+        
         if longestCombo > answer:
             answer = longestCombo
 
