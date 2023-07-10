875fc5098c8ada9c75af10cff0791c56af9f1082
caboodles <sanghyup2000@gmail.com>
2023-07-10 14:52:23 +0900

    cols 1차 시도

--- sanghyup/3085.py
+++ sanghyup/3085.py
@@ -1,3 +1,43 @@
 import sys
 
+def invertMatrix(matrix):
+    matrix = list(map(list, zip(*matrix)))
+    return matrix
+
 n = int(sys.stdin.readline())
+board = []
+
+for _ in range(n):
+    row = list(sys.stdin.readline().rstrip())
+    board.append(row)
+
+answer = 0
+for i in range(n):
+    for _ in range(n):
+        combo = True
+        changed = False
+        longestCombo = 1
+        for j in range(_, n-1):
+            if j-1 >=0:
+                if i-1>=0 and board[i-1][j-1]==board[i][j]:
+                    longestCombo+=1
+                    changed == True
+                elif i+1<n and board[i+1][j-1]==board[i][j]:
+                    longestCombo+=1
+                    changed == True
+            if j+1< n:
+                if board[i][j+1]==board[i][j]:
+                    longestCombo+=1
+                elif changed == False and i-1>=0 and board[i-1][j+1]==board[i][j]:
+                    longestCombo+=1
+                    changed == True
+                elif changed == False and i+1<n and board[i+1][j+1]==board[i][j]:
+                    longestCombo+=1
+                    changed == True
+                else:
+                    break
+            print(i,_, j, board[i][j], longestCombo)
+        if longestCombo > answer:
+            answer = longestCombo
+
+print(answer)
