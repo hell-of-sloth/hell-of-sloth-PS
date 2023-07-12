613916c21bbd6da1f642eba50998bf13bd93475a
caboodles <sanghyup2000@gmail.com>
2023-07-12 16:02:53 +0900

    7/12 업보청산

--- sanghyup/7576.py
+++ sanghyup/7576.py
@@ -5,7 +5,6 @@ cols, rows =  map(int, sys.stdin.readline().split())
 space = cols*rows
 
 box = []
-ripe = set()
 
 q = deque([])
 last = 0
@@ -13,47 +12,29 @@ row = 0
 for _ in range(rows):
     for i, tomato in enumerate(sys.stdin.readline().rstrip().split()):
         tomato = int(tomato)
-        box.append(tomato) # 필요할까?
+        box.append(tomato)
         if tomato == 1:
-            ripe.add(row+i)
             q.append(row+i)
     row+=cols
 
 while q:
-    change = False
     t = q.popleft()
     if t+cols<space:
         if box[t+cols] == 0:
-            change = True
             box[t+cols] = box[t]+1
             q.append(t+cols)
-            ripe.add(t+cols)
     if (t%cols)<(cols-1):
         if box[t+1] == 0:
-            change = True
             box[t+1] = box[t]+1
             q.append(t+1)
-            ripe.add(t+1)
     if (t%cols)>(0):
         if box[t-1] == 0:
-            change = True
             box[t-1] = box[t]+1
             q.append(t-1)
-            ripe.add(t-1)
-    if (t-cols)> 0 :
+    if (t-cols)>= 0 :
         if box[t-cols]==0:
-            change = True
             box[t-cols] = box[t]+1
             q.append(t-cols)
-            ripe.add(t-cols)
-    # if(change):
-    #     for row in range(rows):
-    #         for col in range(cols):
-    #             print(box[row*cols +col], end='')
-    #         print()
-        # print(t,ripe)
-        # print(q)
-        # print()
 answer = 0
 for i in box:
     if i == 0:
