92f014a1939ef08a986978928efd274bdd48d73e
SJY <jun000405@naver.com>
2023-07-15 15:19:40 +0900

    junyoung 0715 자바스크립트 공부

---
+++ javascript_html/string.js
@@ -0,0 +1,15 @@
+// 문자는 큰따옴표나 작은따옴표로 감싸야 한다.
+alert("Hello World");
+alert('Hello World');
+
+// typeof 연산자는 피연산자의 타입을 나타내는 문자열을 반환한다.
+alert(typeof "Hello World"); // string
+alert(typeof 1); // number
+
+// \n 은 줄바꿈을 의미한다.
+
+// 문자의 길이를 알고 싶을 때는 .length 를 사용한다.
+alert("Hello World".length); // 11
+
+// 문자의 인덱스 찾기 indexOf
+alert("Hello World".indexOf("W")); // 6
