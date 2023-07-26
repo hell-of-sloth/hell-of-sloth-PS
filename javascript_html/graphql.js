3cb32e66f0278c7f3efde6a2b8aa59d3304ad268
SJY <jun000405@naver.com>
2023-07-26 19:41:31 +0900

    junyoung 0726 js공부+graphql 뭔지 알아보기

---
+++ javascript_html/graphql.js
@@ -0,0 +1,13 @@
+// 그 대충 봐봤는데
+// graphql -> 원하는 정본만 보내주는 것
+// restful -> 모든 정보를 다 보내주는 것
+
+// Path: hell-of-sloth-PS\javascript_html\graphql.js
+// 그래서 이런식으로 쿼리를 날려서 원하는 정보만 받아올 수 있음
+// query {
+//   movie(id: 1) {
+//     title
+//     medium_cover_image
+//   }
+// }
+
