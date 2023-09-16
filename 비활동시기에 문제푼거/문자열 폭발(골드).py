eb7a8b290e5e771f069b7d18b81b1ff96f23482d
SJY <jun000405@naver.com>
2023-09-16 22:59:06 +0900

    0916(2)

--- 비활동시기에 문제푼거/문자열 폭발(골드).py
+++ 비활동시기에 문제푼거/문자열 폭발(골드).py
@@ -3,17 +3,24 @@ import sys
 string = sys.stdin.readline().rstrip()
 explosion_str = sys.stdin.readline().rstrip()
 
-def string_explosion(): # 폭발은 예술이다
+def string_explosion():
     global string, explosion_str
+
+    stack = []  # 스택을 사용하여 문자열을 관리
+
+    for char in string:
+        stack.append(char)  # 스택에 문자 추가
+
+        # 현재 스택의 끝 문자열이 폭발 문자열의 마지막 문자와 같다면 확인
+        if char == explosion_str[-1] and ''.join(stack[-len(explosion_str):]) == explosion_str:
+            # 폭발 문자열 제거
+            del stack[-len(explosion_str):]
+
+    result = ''.join(stack)
     
-    while True:
-        if explosion_str not in string: # 폭발 문자열이 더이상 없으면 종료
-            break
-        string = string.replace(explosion_str, '') # 폭발 문자열 제거
-        
-    if string == '': # 빈 문자열이면 FRULA 출력
+    if not result:
         print('FRULA')
-    else:            # 아니면 문자열 출력
-        print(string)
-        
+    else:
+        print(result)
+
 string_explosion()
