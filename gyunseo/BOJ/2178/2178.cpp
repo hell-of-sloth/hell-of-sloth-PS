7c0b30a8516c58295ba0550af63fcb7fb2dfd12e
Gyunseo Lee <rbstj0311@naver.com>
2024-01-26 17:39:53 +0900

    gyunseo: BOJ-2178

--- gyunseo/BOJ/2178/2178.cpp
+++ gyunseo/BOJ/2178/2178.cpp
@@ -4,35 +4,38 @@
 #define endl '\n'
 using namespace std;
 const int MAX = 100;
-queue<tuple<int, int, int>> q;
-int board[MAX][MAX];
-int is_visited[MAX][MAX];
+queue<pair<int, int>> q;
+string board[MAX];
+int is_visited[MAX][MAX], dist[MAX][MAX];
 int di[4] = {-1, 1, 0, 0};
 int dj[4] = {0, 0, -1, 1};
-int N,M;
+int N, M;
+
 bool OOB(int i, int j) {
   if (i < 0 || i >= N) return true;
   if (j < 0 || j >= M) return true;
   return false;
 }
+
 void ReadUserInput() {
   cin >> N >> M;
   for (int i = 0; i < N; i++) {
-    for (int j = 0; j < M; j++) {
-      cin >> board[i][j];
-    }
+    cin >> board[i];
   }
 }
 
 void Solve() {
-  int cur_i, cur_j, cur_cnt;
-  q.push(make_tuple(0, 0, 1));
+  int cur_i, cur_j;
+  q.push({0, 0});
   while(!q.empty()) {
-    tie(cur_i, cur_j, cur_cnt) = q.front();
+    tie(cur_i, cur_j) = q.front();
     q.pop();
+    if (is_visited[cur_i][cur_j]) continue;
     is_visited[cur_i][cur_j] = 1;
+    //cout << cur_i << " " << cur_j << "visited!" << endl;
     if (cur_i == (N - 1) && cur_j == (M - 1)) {
-      cout << cur_cnt << endl;
+      //cout << cur_i << " " << cur_j << endl;
+      cout << dist[cur_i][cur_j] + 1 << endl;
       return;
     }
     for (int k = 0; k < 4; k++) {
@@ -41,8 +44,10 @@ void Solve() {
       next_j = cur_j + dj[k];
       if (OOB(next_i, next_j)) continue;
       if (is_visited[next_i][next_j] == 1) continue;
-      if (board[next_i][next_j] == 0) continue;
-      q.push(make_tuple(next_i, next_j, cur_cnt + 1));
+      if (board[next_i][next_j] == '0') continue;
+      if (dist[next_i][next_j] >= 1) continue;
+      dist[next_i][next_j] = dist[cur_i][cur_j] + 1;
+      q.push({next_i, next_j});
     }
   }
 }
