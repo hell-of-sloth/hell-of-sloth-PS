8b07d4493b4e996071eb8a3b6b176182c32bf24f
Gyunseo Lee <rbstj0311@naver.com>
2024-02-09 23:47:15 +0900

    fix: format

--- gyunseo/BOJ/2178/2178.cpp
+++ gyunseo/BOJ/2178/2178.cpp
@@ -13,44 +13,52 @@ int dj[4] = {0, 0, -1, 1};
 int N, M;
 
 bool OOB(int i, int j) {
-  if (i < 0 || i >= N) return true;
-  if (j < 0 || j >= M) return true;
-  return false;
+    if (i < 0 || i >= N)
+        return true;
+    if (j < 0 || j >= M)
+        return true;
+    return false;
 }
 
 void ReadUserInput() {
-  cin >> N >> M;
-  for (int i = 0; i < N; i++) {
-    cin >> board[i];
-  }
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        cin >> board[i];
+    }
 }
 
 void Solve() {
-  int cur_i, cur_j;
-  q.push({0, 0});
-  dist[0][0] = 1;
-  while(!q.empty()) {
-    tie(cur_i, cur_j) = q.front();
-    q.pop();
-    if (cur_i == (N - 1) && cur_j == (M - 1)) {
-      cout << dist[cur_i][cur_j] << endl;
-      return;
+    int cur_i, cur_j;
+    q.push({0, 0});
+    dist[0][0] = 1;
+    while (!q.empty()) {
+        tie(cur_i, cur_j) = q.front();
+        q.pop();
+        if (cur_i == (N - 1) && cur_j == (M - 1)) {
+            cout << dist[cur_i][cur_j] << endl;
+            return;
+        }
+        for (int k = 0; k < 4; k++) {
+            int next_i, next_j;
+            next_i = cur_i + di[k];
+            next_j = cur_j + dj[k];
+            if (OOB(next_i, next_j))
+                continue;
+            if (board[next_i][next_j] == '0')
+                continue;
+            if (dist[next_i][next_j] >= 1)
+                continue;
+            dist[next_i][next_j] = dist[cur_i][cur_j] + 1;
+            q.push({next_i, next_j});
+        }
     }
-    for (int k = 0; k < 4; k++) {
-      int next_i, next_j;
-      next_i = cur_i + di[k];
-      next_j = cur_j + dj[k];
-      if (OOB(next_i, next_j)) continue;
-      if (board[next_i][next_j] == '0') continue;
-      if (dist[next_i][next_j] >= 1) continue;
-      dist[next_i][next_j] = dist[cur_i][cur_j] + 1;
-      q.push({next_i, next_j});
-    }
-  }
 }
+
 int main() {
-  ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-  ReadUserInput();
-  Solve();
-  return 0;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ReadUserInput();
+    Solve();
+    return 0;
 }
