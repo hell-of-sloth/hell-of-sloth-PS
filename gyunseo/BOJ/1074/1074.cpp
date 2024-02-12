130267a7fde2f33b45e67af9d4f17cdf715f63b8
Gyunseo Lee <rbstj0311@naver.com>
2024-02-13 01:28:21 +0900

    gyunseo: format the code

--- gyunseo/BOJ/1074/1074.cpp
+++ gyunseo/BOJ/1074/1074.cpp
@@ -5,24 +5,27 @@ using namespace std;
 int N, r, c;
 
 int f(int i, int j, int n) {
-    if (n == 0) return 0;
+    if (n == 0)
+        return 0;
     int half_size = (1 << n - 1);
-    if (i < half_size && j < half_size) return f(i, j, n - 1);
-    if (i < half_size && j >= half_size) return half_size * half_size + f(i, j - half_size, n - 1); 
-    if (i >= half_size && j < half_size) return 2 * half_size * half_size + f(i - half_size, j, n - 1);
-    else return 3 * half_size * half_size + f(i - half_size, j - half_size, n - 1);
+    if (i < half_size && j < half_size)
+        return f(i, j, n - 1);
+    if (i < half_size && j >= half_size)
+        return half_size * half_size + f(i, j - half_size, n - 1);
+    if (i >= half_size && j < half_size)
+        return 2 * half_size * half_size + f(i - half_size, j, n - 1);
+    else
+        return 3 * half_size * half_size +
+               f(i - half_size, j - half_size, n - 1);
 }
-void solve() {
-    cout << f(r, c, N) << endl;
-}
+void solve() { cout << f(r, c, N) << endl; }
 
-void read_user_input() {
-    cin >> N >> r >> c;
-}
-int main() { 
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+void read_user_input() { cin >> N >> r >> c; }
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     read_user_input();
     solve();
-    return 0; 
+    return 0;
 }
-
