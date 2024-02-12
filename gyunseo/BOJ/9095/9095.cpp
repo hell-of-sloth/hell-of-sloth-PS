96cb6768649cf43a9517567f3fff7149090e9da5
Gyunseo Lee <rbstj0311@naver.com>
2024-02-13 01:30:12 +0900

    gyunseo: format the code

--- gyunseo/BOJ/9095/9095.cpp
+++ gyunseo/BOJ/9095/9095.cpp
@@ -5,15 +5,13 @@ using namespace std;
 
 int T, n;
 int dp_table[12];
-void solve(int num) {
-    cout << dp_table[num] << endl; 
-}
+void solve(int num) { cout << dp_table[num] << endl; }
 void read_user_input() {
     dp_table[1] = 1;
     dp_table[2] = 2;
     dp_table[3] = 4;
     for (int i = 4; i <= 11; i++) {
-        dp_table[i] = dp_table[i - 1] + dp_table[i - 2] + dp_table[i - 3]; 
+        dp_table[i] = dp_table[i - 1] + dp_table[i - 2] + dp_table[i - 3];
     }
     cin >> T;
     for (int i = 0; i < T; i++) {
@@ -23,7 +21,9 @@ void read_user_input() {
 }
 
 int main() {
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     read_user_input();
     return 0;
 }
