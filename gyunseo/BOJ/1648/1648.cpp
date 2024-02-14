d216c61f4e5fb34aea371552d6a94d78413be553
Gyunseo Lee <rbstj0311@naver.com>
2024-02-14 13:01:06 +0900

    gyunseo: BOJ-1648 나중에 풀기

---
+++ gyunseo/BOJ/1648/1648.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#define endl '\n'
+
+using namespace std;
+const int MAX = 14;
+int N, M;
+void solve() {
+}
+
+void read_user_input() {
+    cin >> N >> M;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    read_user_input();
+    solve();
+    return 0;
+}
