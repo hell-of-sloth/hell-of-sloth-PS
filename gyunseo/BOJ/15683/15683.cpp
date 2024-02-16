8e232076ea057273fb596c0f380bf40995bb3255
Gyunseo Lee <rbstj0311@naver.com>
2024-02-16 15:15:41 +0900

    gyunseo: BOJ-15683 another approach

--- gyunseo/BOJ/15683/15683.cpp
+++ gyunseo/BOJ/15683/15683.cpp
@@ -1,320 +1,152 @@
 #include <iostream>
 #include <tuple>
 #include <vector>
+#include <cassert>
 #define endl '\n'
+#define ASSERT(exp, msg) assert(exp && msg)
+#define F first
+#define S second
+
 using namespace std;
+
 const int MAX = 8;
 const int INF = (int)1e9;
-
-// 방향
-enum eDir
-{
-    Up,
-    Down,
-    Left,
-    Right
-};
-
-// CCTV type
-enum eCamType
-{
-    One = 1,
-    Two,
-    Three,
-    Four,
-    Five
-};
-// 상하좌우
-int di[4] = {-1, 1, 0, 0};
-int dj[4] = {0, 0, -1, 1};
+const int MARK = 7;
 int N, M, ans = INF;
-int matrix[MAX][MAX];
-vector<pair<int, int>> cam_pos;
-vector<int> cam_type;
-int cam_cnt = 0;
+int MATRIX[MAX + 1][MAX + 1];
+int matrix[MAX + 1][MAX + 1];
+enum e_dir {UP, RIGHT, DOWN, LEFT};
+int di[4] = {-1, 0, 1, 0};
+int dj[4] = {0, 1, 0, -1};
+vector<pair<int, int>> cam_poses;
 
-// out of bound
-bool OOB(int i, int j)
-{
-    if (i < 0 || i >= N)
-        return true;
-    if (j < 0 || j >= M)
-        return true;
+bool OOB(int i, int j) {
+    if (i < 0 || i >= N) return true;
+    if (j < 0 || j >= M) return true;
     return false;
 }
-// CCTV 최대 개수 8개
-// 사각지대를 카운트해서 반환
-int GetZeroCount()
-{
+
+void update_matrix(int i, int j, int dir) {
+    bool iteration_trigger = true;
+    int ni, nj;
+    dir = dir % 4;
+    ASSERT(dir >= 0 && dir < 4, "dir is out of bound");
+    // init the ni, nj values
+    ni = i; 
+    nj = j; 
+    while (iteration_trigger) {
+        ni = ni + di[dir];
+        nj = nj + dj[dir];
+        if (OOB(ni, nj)) {
+            iteration_trigger = false;
+            continue;
+        }
+        if (matrix[ni][nj] == 6) {
+            iteration_trigger = false;
+            continue;
+        }
+        // 만약에 [ni][nj]에 감시 카메라가 있다면 그냥 지나가기
+        if (matrix[ni][nj] >= 1 && matrix[ni][nj] <= 5) continue;
+        // 만약에 [ni][nj]에 미리 누가 마킹을 했다면 그냥 지나가기
+        if (matrix[ni][nj] == MARK) continue;
+        // 위 조건 어느 것도 해당이 안된다면 0이라는 건데, 그 때는 마킹하기
+        if (matrix[ni][nj] == 0) {
+            matrix[ni][nj] = MARK; 
+            continue;
+        }
+        // 엥 이것도 아니면 오류를 낸다 
+        ASSERT(false, "you cannot reach here in update_matrix!");
+    }
+}
+
+int pow(int n, int k) {
+    int ret  = 1;
+    for (int i = 0; i < k; i++) {
+        ret *= n;
+    }
+    return ret;
+}
+
+void copy_the_init_state_of_MATRIX() {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            matrix[i][j] = MATRIX[i][j];
+        }
+    }
+}
+
+int get_num_zeroes() {
     int cnt = 0;
-    // O(NM) 최악 64
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < M; j++)
-        {
-            if (matrix[i][j] == 0)
-                cnt++;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            if (matrix[i][j] == 0) cnt++;
         }
     }
     return cnt;
 }
 
-void UpdateMatrix(int cur_i, int cur_j, int dir)
-{
-    while (true)
-    {
-        cur_i += di[dir];
-        cur_j += dj[dir];
-        // matrix 범위 밖이거나, 벽을 만나면 리턴
-        if (OOB(cur_i, cur_j) || matrix[cur_i][cur_j] == 6)
-            return;
-        // cam이 있거나, 이미 -1로 업데이트 돼있다면, 건너 뛰기
-        if (matrix[cur_i][cur_j] >= 1 && matrix[cur_i][cur_j] <=5)
-            continue;
-        matrix[cur_i][cur_j] += -1;
-    }
+void solve() {
+
+    int num_cam = cam_poses.size();
+    //cout << "num_cam: " << num_cam << endl;
+    int brute_cases = pow(4, num_cam);
+    //cout << "brute_cases: " << brute_cases << endl;
+    int cur_dir, cur_cam_type, tmp_case, cur_cam_i, cur_cam_j;
+    for (int brute_case = 0; brute_case < brute_cases; brute_case++) {
+        copy_the_init_state_of_MATRIX();
+        // 4진법으로 모든 케이스를 cover
+        tmp_case = brute_case;
+        for (int i = 0; i < num_cam; i++) {
+            cur_dir = tmp_case % 4;
+            tie(cur_cam_i, cur_cam_j) = cam_poses[i]; 
+            cur_cam_type = MATRIX[cur_cam_i][cur_cam_j];
+            ASSERT(cur_cam_type >= 1 && cur_cam_type <= 5, "cur_cam_type is out of bound!");
+            ASSERT(cur_dir >= UP && cur_dir <= LEFT, "cur_dir is out of bound!");
+            tmp_case = tmp_case / 4;
+            switch (cur_cam_type) {
+                case 1:
+                    update_matrix(cur_cam_i, cur_cam_j, cur_dir);
+                    break;
+                case 2:
+                    update_matrix(cur_cam_i, cur_cam_j, cur_dir);
+                    update_matrix(cur_cam_i, cur_cam_j, cur_dir + 2);
+                    break;
+                case 3:
+                    update_matrix(cur_cam_i, cur_cam_j, cur_dir);
+                    update_matrix(cur_cam_i, cur_cam_j, cur_dir + 1);
+                    break;
+                case 4:
+                    update_matrix(cur_cam_i, cur_cam_j, cur_dir);
+                    update_matrix(cur_cam_i, cur_cam_j, cur_dir + 1);
+                    update_matrix(cur_cam_i, cur_cam_j, cur_dir + 3);
+                    break;
+                case 5:
+                    update_matrix(cur_cam_i, cur_cam_j, cur_dir);
+                    update_matrix(cur_cam_i, cur_cam_j, cur_dir + 1);
+                    update_matrix(cur_cam_i, cur_cam_j, cur_dir + 2);
+                    update_matrix(cur_cam_i, cur_cam_j, cur_dir + 3);
+                    break;
+            }
+        }
+        int tmp_ans = get_num_zeroes();
+        if (tmp_ans < ans) ans = tmp_ans;
+    } 
 }
 
-void UndoUpdateMatrix(int cur_i, int cur_j, int dir)
-{
-    while (true)
-    {
-        cur_i += di[dir];
-        cur_j += dj[dir];
-        // matrix 범위 밖이거나, 벽을 만나면 리턴
-        if (OOB(cur_i, cur_j) || matrix[cur_i][cur_j] == 6)
-            return;
-        if (matrix[cur_i][cur_j] <= -1)
-            matrix[cur_i][cur_j] += +1;
-    }
-}
-void FireLaser(pair<int, int> &cur_cam_pos, int cur_cam_type, int cur_cam_dir)
-{
-    int cur_i, cur_j;
-    tie(cur_i, cur_j) = cur_cam_pos;
-    switch (cur_cam_type)
-    {
-    case One:
-        UpdateMatrix(cur_i, cur_j, cur_cam_dir);
-        break;
-    case Two:
-        UpdateMatrix(cur_i, cur_j, cur_cam_dir);
-        switch (cur_cam_dir)
-        {
-        case Up:
-            UpdateMatrix(cur_i, cur_j, Down);
-            break;
-        case Down:
-            UpdateMatrix(cur_i, cur_j, Up);
-            break;
-        case Left:
-            UpdateMatrix(cur_i, cur_j, Right);
-            break;
-        case Right:
-            UpdateMatrix(cur_i, cur_j, Left);
-            break;
-        }
-        break;
-    case Three:
-        UpdateMatrix(cur_i, cur_j, cur_cam_dir);
-        switch (cur_cam_dir)
-        {
-        case Up:
-            UpdateMatrix(cur_i, cur_j, Right);
-            break;
-        case Down:
-            UpdateMatrix(cur_i, cur_j, Left);
-            break;
-        case Left:
-            UpdateMatrix(cur_i, cur_j, Up);
-            break;
-        case Right:
-            UpdateMatrix(cur_i, cur_j, Down);
-            break;
-        }
-        break;
-    case Four:
-        UpdateMatrix(cur_i, cur_j, cur_cam_dir);
-        switch (cur_cam_dir)
-        {
-        case Up:
-            UpdateMatrix(cur_i, cur_j, Left);
-            UpdateMatrix(cur_i, cur_j, Right);
-            break;
-        case Down:
-            UpdateMatrix(cur_i, cur_j, Right);
-            UpdateMatrix(cur_i, cur_j, Left);
-            break;
-        case Left:
-            UpdateMatrix(cur_i, cur_j, Down);
-            UpdateMatrix(cur_i, cur_j, Up);
-            break;
-        case Right:
-            UpdateMatrix(cur_i, cur_j, Up);
-            UpdateMatrix(cur_i, cur_j, Down);
-            break;
-        }
-        break;
-    case Five:
-        UpdateMatrix(cur_i, cur_j, Up);
-        UpdateMatrix(cur_i, cur_j, Down);
-        UpdateMatrix(cur_i, cur_j, Left);
-        UpdateMatrix(cur_i, cur_j, Right);
-        break;
-    }
-}
-
-void UndoFireLaser(pair<int, int> &cur_cam_pos, int cur_cam_type, int cur_cam_dir)
-{
-    int cur_i, cur_j;
-    tie(cur_i, cur_j) = cur_cam_pos;
-    switch (cur_cam_type)
-    {
-    case One:
-        UndoUpdateMatrix(cur_i, cur_j, cur_cam_dir);
-        break;
-    case Two:
-        UndoUpdateMatrix(cur_i, cur_j, cur_cam_dir);
-        switch (cur_cam_dir)
-        {
-        case Up:
-            UndoUpdateMatrix(cur_i, cur_j, Down);
-            break;
-        case Down:
-            UndoUpdateMatrix(cur_i, cur_j, Up);
-            break;
-        case Left:
-            UndoUpdateMatrix(cur_i, cur_j, Right);
-            break;
-        case Right:
-            UndoUpdateMatrix(cur_i, cur_j, Left);
-            break;
-        }
-        break;
-    case Three:
-        UndoUpdateMatrix(cur_i, cur_j, cur_cam_dir);
-        switch (cur_cam_dir)
-        {
-        case Up:
-            UndoUpdateMatrix(cur_i, cur_j, Right);
-            break;
-        case Down:
-            UndoUpdateMatrix(cur_i, cur_j, Left);
-            break;
-        case Left:
-            UndoUpdateMatrix(cur_i, cur_j, Up);
-            break;
-        case Right:
-            UndoUpdateMatrix(cur_i, cur_j, Down);
-            break;
-        }
-        break;
-    case Four:
-        UndoUpdateMatrix(cur_i, cur_j, cur_cam_dir);
-        switch (cur_cam_dir)
-        {
-        case Up:
-            UndoUpdateMatrix(cur_i, cur_j, Left);
-            UndoUpdateMatrix(cur_i, cur_j, Right);
-            break;
-        case Down:
-            UndoUpdateMatrix(cur_i, cur_j, Right);
-            UndoUpdateMatrix(cur_i, cur_j, Left);
-            break;
-        case Left:
-            UndoUpdateMatrix(cur_i, cur_j, Down);
-            UndoUpdateMatrix(cur_i, cur_j, Up);
-            break;
-        case Right:
-            UndoUpdateMatrix(cur_i, cur_j, Up);
-            UndoUpdateMatrix(cur_i, cur_j, Down);
-            break;
-        }
-        break;
-    case Five:
-        UndoUpdateMatrix(cur_i, cur_j, Up);
-        UndoUpdateMatrix(cur_i, cur_j, Down);
-        UndoUpdateMatrix(cur_i, cur_j, Left);
-        UndoUpdateMatrix(cur_i, cur_j, Right);
-        break;
-    }
-}
-void PrintMatrix() {
+void read_user_input() {
+    cin >> N >> M;
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
-            cout << matrix[i][j] << " ";
-        }cout << endl;
-    }
-}
-// 상태 공간 트리를 DFS로 순회한다.
-// level이 N번째 캠을 의미
-void DFS(int level, int direction)
-{
-    if (level >= cam_cnt) {
-        int tmp_ans = GetZeroCount();
-        if (tmp_ans < ans) {
-            // cout << "tmp_ans: " << tmp_ans << endl;
-            // cout << "-------------\n";
-            // PrintMatrix();
-            ans = tmp_ans;
-        }
-        return;
-    }
-    // level번째 캠을 direction 방향으로 돌린다.
-    // cout << "cur_level_cam_pos: " << cam_pos[level].first << " " << cam_pos[level].second << endl;
-    // cout << "cam_type:" << cam_type[level] << endl;
-    // cout << "cur_cam_dir: " << direction << endl;
-    FireLaser(cam_pos[level], cam_type[level], direction);
-    // PrintMatrix();
-    // 0이 상, 1이 하, 2가 좌, 3이 우
-    for (int n_dir = Up; n_dir <= Right; n_dir++)
-    {
-        DFS(level + 1, n_dir);
-    }
-    // undo latest laser fire
-    UndoFireLaser(cam_pos[level], cam_type[level], direction);
-}
-// 문제를 푼다.
-void Solve()
-{
-    int cur_i, cur_j;
-    // 4의 8승 65000정도이다.
-    // pos는 최대 8개이다.
-    // 1000ms이니깐, 브루트포스로 풀면된다.
-    for (int dir = Up; dir <= Right; dir++)
-    {
-        DFS(0, dir);
-    }
-    cout << ans << endl;
-}
-// user의 cli input을 읽어서 변수에 집어 넣는다.
-void ReadUserInput()
-{
-    cin >> N >> M;
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < M; j++)
-        {
-            cin >> matrix[i][j];
-            if (matrix[i][j] < 1)
-                continue;
-            if (matrix[i][j] > 5)
-                continue;
-            // 1~5이면 cctv
-            cam_pos.push_back(make_pair(i, j));
-            cam_type.push_back(matrix[i][j]);
-            cam_cnt++;
+            cin >> MATRIX[i][j];
+            if (MATRIX[i][j] >= 1 && MATRIX[i][j] <= 5) cam_poses.push_back({i, j});
         }
     }
-    // cout << "cam_cnt: " << cam_cnt << endl;
 }
 
-int main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    ReadUserInput();
-    Solve();
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    read_user_input();
+    solve();
+    cout << ans << endl;
     return 0;
 }
