6cc045462146afd6c64aa259b723aa7a9712deca
Gyunseo Lee <rbstj0311@naver.com>
2024-02-17 18:09:56 +0900

    gyunseo: format and delete unnecessary variables

--- gyunseo/BOJ/9663/9663.cpp
+++ gyunseo/BOJ/9663/9663.cpp
@@ -1,15 +1,14 @@
-#include <iostream>
-#include <cassert>
-#include <vector>
 #include <algorithm>
+#include <cassert>
 #include <cstdlib>
+#include <iostream>
+#include <vector>
 #define endl '\n'
-#define ASSERT(exp, msg) assert(exp & msg)
+#define ASSERT(exp, msg) assert(exp &msg)
 using namespace std;
 
 const int MAX = 15;
 int board[MAX + 1][MAX + 1];
-int is_issued[MAX + 1][MAX + 1];
 vector<int> is_issued_col;
 int N, ans;
 
@@ -20,7 +19,8 @@ bool is_in_diagonal_path(int cur_col, int cur_row) {
         int col = is_issued_col[i];
         int row = i;
         // int abs는 cstdlib에 있음
-        if (abs(cur_row - row) == abs(cur_col - col)) return true; 
+        if (abs(cur_row - row) == abs(cur_col - col))
+            return true;
     }
     return false;
 }
@@ -30,8 +30,11 @@ void dfs(int col, int row) {
         return;
     }
     for (int i = 0; i < N; i++) {
-        if (find(is_issued_col.begin(), is_issued_col.end(), i) != is_issued_col.end()) continue;
-        if (is_in_diagonal_path(i, row + 1)) continue;
+        if (find(is_issued_col.begin(), is_issued_col.end(), i) !=
+            is_issued_col.end())
+            continue;
+        if (is_in_diagonal_path(i, row + 1))
+            continue;
         is_issued_col.push_back(i);
         dfs(i, row + 1);
         is_issued_col.pop_back();
@@ -47,12 +50,12 @@ void solve() {
     cout << ans << endl;
 }
 
-void read_user_input() {
-    cin >> N;
-}
+void read_user_input() { cin >> N; }
 
 int main() {
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     read_user_input();
     solve();
     return 0;
