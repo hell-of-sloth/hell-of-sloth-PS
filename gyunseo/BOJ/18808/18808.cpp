ab8c78800cb77bf527187c7bc453dbf96491d83a
Gyunseo Lee <rbstj0311@naver.com>
2024-02-17 17:37:03 +0900

    gyunseo: boj 18808 added testcases

---
+++ gyunseo/BOJ/18808/18808.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cassert>
+
+#define endl '\n'
+#define ASSERT(exp, msg) assert(exp && msg)
+using namespace std;
+
+void solve() {
+}
+
+void read_user_input() {
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    read_user_input();
+    solve();
+    return 0;
+}
