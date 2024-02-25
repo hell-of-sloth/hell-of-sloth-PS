e4580bf6b9c18de4f871375869aafd0b23167e21
Gyunseo Lee <rbstj0311@naver.com>
2024-02-25 23:58:29 +0900

    gyunseo: BOJ-1149

--- gyunseo/BOJ/1149/1149.cpp
+++ gyunseo/BOJ/1149/1149.cpp
@@ -1,49 +1,42 @@
 #include <iostream>
+#include <cassert>
 #define endl '\n'
+#define ASSERT(exp, msg) assert(exp && msg)
 using namespace std;
-
 const int MAX = 1000;
 int N;
-int rgb_cost[MAX + 1][3], dp[MAX + 1][3];
 
-void readInput()
-{
+enum e_color {
+    R,
+    G,
+    B
+};
+
+// R = 0, G = 1, B = 2
+int RGB[MAX + 1][3], dp[MAX + 1][3];
+
+void solve() {
+    dp[1][R] = RGB[1][R];
+    dp[1][G] = RGB[1][G];
+    dp[1][B] = RGB[1][B];
+    for (int i = 2; i <= N; i++) {
+        dp[i][R] = min(dp[i - 1][G], dp[i - 1][B]) + RGB[i][R];
+        dp[i][G] = min(dp[i - 1][R], dp[i - 1][B]) + RGB[i][G];
+        dp[i][B] = min(dp[i - 1][R], dp[i - 1][G]) + RGB[i][B];
+    }
+    cout << min(min(dp[N][R], dp[N][G]), dp[N][B]) << endl;
+}
+
+void read_user_input() {
     cin >> N;
-    for (int i = 1; i <= N; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            cin >> rgb_cost[i][j];
-        }
+    for (int i = 1; i <= N; i++) {
+        cin >> RGB[i][R] >> RGB[i][G] >> RGB[i][B];
     }
 }
 
-void DP()
-{
-    for (int i = 0; i < 3; i++)
-    {
-        dp[1][i] = rgb_cost[1][i];
-    }
-    for (int i = 2; i <= N; i++)
-    {
-        dp[i][0] = rgb_cost[i][0] + min(dp[i - 1][1], dp[i - 1][2]);
-        dp[i][1] = rgb_cost[i][1] + min(dp[i - 1][0], dp[i - 1][2]);
-        dp[i][2] = rgb_cost[i][2] + min(dp[i - 1][0], dp[i - 1][1]);
-    }
-}
-
-void solve()
-{
-    DP();
-    cout << min(min(dp[N][0], dp[N][1]), dp[N][2]) << endl;
-}
-
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    readInput();
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    read_user_input();
     solve();
     return 0;
 }
