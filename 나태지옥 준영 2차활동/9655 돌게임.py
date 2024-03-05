c1efbc8000c0276c90aaf2aec98e39d0a095ad29
SJY <jun000405@naver.com>
2024-03-06 00:47:04 +0900

    feat: junyoung 0305

---
+++ 나태지옥 준영 2차활동/9655 돌게임.py
@@ -0,0 +1,8 @@
+import sys
+
+N = int(sys.stdin.readline().strip())
+
+if N % 2 == 0:
+    print("CY")
+else:
+    print("SK")
---
+++ 나태지옥 준영 2차활동/9655.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+
+    cin >> N;
+
+    if (N % 2 == 0) {
+        cout << "CY";
+    } else {
+        cout << "SK";
+    }
+
+    return 0;
+}
