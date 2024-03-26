e6389981cd6e4a84eee9d1ebcfba0f53fec02e94
SJY <jun000405@naver.com>
2024-03-26 16:41:28 +0900

    feat: junyoung 0326

---
+++ 나태지옥 준영 2차활동/1의 개수 세기.py
@@ -0,0 +1,27 @@
+import sys
+
+# 이진수 변환 리스트 이용
+
+A, B = map(int, sys.stdin.readline().split())
+
+dp = [0] * 60
+dp[1] = 1
+
+def bit_bungi(value, n):
+    return value * 2 + 2**(n-1)
+
+def bit_cal(value):
+    result = 0
+    bin_num = bin(value)[2:]
+    while bin_num:
+        if bin_num[0] == '1':
+            result += dp[len(bin_num)-1]
+            result += int(bin_num, 2) - 2**(len(bin_num)-1) + 1
+        bin_num = bin_num[1:]
+    return result
+            
+
+for i in range(2, 60):
+    dp[i] = bit_bungi(dp[i-1], i)
+    
+print(bit_cal(B) - bit_cal(A-1))
