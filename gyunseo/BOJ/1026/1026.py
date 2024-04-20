638768d312ed13d1f7542c17b22a74ca22ab402e
Gyunseo Lee <rbstj0311@naver.com>
2024-04-20 16:18:43 +0900

    gyunseo: BOJ-1026 refactor

--- gyunseo/BOJ/1026/1026.py
+++ gyunseo/BOJ/1026/1026.py
@@ -1,13 +1,16 @@
 import sys
+
 input = sys.stdin.readline
 MAX_LEN = 50
 MAX_VAL = 100
 
-def S():
+
+def get_S():
     ret = 0
     for i in range(N):
         ret += A[i] * B[i]
     return ret
+
 
 if __name__ == "__main__":
     N = int(input().rstrip())
@@ -16,7 +19,6 @@ if __name__ == "__main__":
     # print("A: ", A)
     # print("B: ", B)
     # A가 B의 계수라고 생각하면 된다
-    A.sort(key = lambda x: -x)
+    A.sort(key=lambda x: -x)
     B.sort()
-    print(S())
-
+    print(get_S())
