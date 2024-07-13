3840593a71e3e46e0cc0c2e34ae5b8c4fc030f52
Gyunseo Lee <rbstj0311@naver.com>
2024-07-13 16:46:46 +0000

    gyunseo: BOJ-2529 permutations로 풀었음

--- gyunseo/BOJ/2529/2529.py
+++ gyunseo/BOJ/2529/2529.py
@@ -3,57 +3,42 @@ from itertools import permutations
 
 input = sys.stdin.readline
 
-
-# def DFS(level, seq_str):
-#     global maxStr, minStr
-#     if level == K:
-#         seq_str_int = int(seq_str)
-#         if seq_str_int > int(maxStr):
-#             maxStr = seq_str
-#         if seq_str_int < int(minStr):
-#             minStr = seq_str
-#         return
-#     for i in range(10):
-#         if isUsed[i]:
-#             continue
-#         if not eval(f"{seq_str[-1]} {operators[level]} {i}"):
-#             continue
-#         isUsed[i] = True
-#         DFS(i, seq_str + str(i))
-#         isUsed[i] = False
-
-
+def check(a, b, op):
+    if op == "<":
+        return a < b
+    else:
+        return a > b
 if __name__ == "__main__":
     K = int(input().strip())
-    operators = [*input().strip().split()]
-    # print(operators)
-    nums = [*range(10)]
-    isUsed = [False for _ in range(10)]
-    maxStr = "0"
-    minStr = "9999999999"
-    # for i in range(10):
-    #     isUsed[i] = True
-    #     DFS(0, str(i))
-    #     isUsed[i] = False
-    # print(nums)
-    candSeqList = permutations(nums, K + 1)
-    # print(candSeqList)
+    operators = input().strip().split()
+ 
+    maxSeqSum = 0
+    minSeqSum = 9999999999
+    maxSeq, minSeq = None, None
+    seqCandsGen = permutations(range(10), K + 1)
 
-    for candSeq in candSeqList:
+    for candSeq in seqCandsGen:
         isValid = True
         for i, operator in enumerate(operators):
-            toBeEvaluated = operator.join(map(str, [candSeq[i], candSeq[i + 1]]))
-            # print(toBeEvaluated)
-            if not eval(toBeEvaluated):
+            if not check(candSeq[i], candSeq[i + 1], operator):
                 isValid = False
                 break
 
         if isValid:
-            candSeqStr = "".join(map(str, candSeq))
-            candSeqInt = int(candSeqStr)
-            if candSeqInt > int(maxStr):
-                maxStr = candSeqStr
-            if candSeqInt < int(minStr):
-                minStr = candSeqStr
-    print(maxStr)
-    print(minStr)
+            seqSum = 0
+            factor = 1
+            for idx in range(K, -1, -1):
+                seqSum += candSeq[idx] * factor
+                factor *= 10
+            # print(f"seqSum: {seqSum}")
+            if seqSum > maxSeqSum:
+                maxSeq = candSeq
+                maxSeqSum = seqSum
+            if seqSum < minSeqSum:
+                minSeq = candSeq
+                minSeqSum = seqSum
+            # print(f"minSeq: {minSeq}")
+
+    # print(maxSeq, minSeq)
+    print("".join(map(str, maxSeq)))
+    print("".join(map(str, minSeq)))
