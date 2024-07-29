91761c3a11efdf0e550587cc841c52543a71ed60
Gyunseo Lee <rbstj0311@naver.com>
2024-07-29 11:52:03 +0000

    gyunseo: BOJ-1920 하는 중

--- gyunseo/BOJ/1920/1920.cpp
+++ gyunseo/BOJ/1920/1920.cpp
@@ -1,49 +1,50 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define endl '\n'
-#define watch(x) cout << (#x) << " is " << x << endl
-#define FOR(s, e) for (int i = s; i < e; i++)
-#define VI vector<int>
-int N, M;
-VI V;
+#include <algorithm>
+#include <iostream>
+#include <vector>
 
-bool num_exists(int num) {
-    int s = 0, e = V.size() - 1;
-    while (s <= e) {
-        int m = (s + e) / 2;
-        if (num == V[m]) {
-            return true;
-        }
-        if (num > V[m]) {
-            s = m + 1;
-            continue;
-        }
-        if (num < V[m]) {
-            e = m - 1;
-            continue;
-        }
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#define endl '\n'
+#define watch cout << (#x) << ": " << (x)
+
+#define REP(idx, end) for (auto(idx) = 0; (idx) < (end); ++(idx))
+#define ALL(v) (v).begin(), (v).end()
+#define EACH(e, v) for (auto &(e) : (v))
+#define PB push_back
+using namespace std;
+
+int N, M;
+vector<int> numVec, queryVec;
+
+int lowerBound(const vector<int> &v, const int target) {
+    int st = -1, en = v.size(), mid;
+    while (st + 1 < en) {
+        mid = (st + en) / 2;
+        if (!(v[mid] >= target))
+            st = mid;
+        else
+            en = mid;
     }
-    return false;
+    return en;
 }
+
+bool cmp(const int &a, const int &b) { return a < b; }
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+    fastio;
     cin >> N;
-    FOR(0, N) {
+    REP(i, N) {
         int e;
         cin >> e;
-        V.push_back(e);
+        numVec.PB(e);
     }
-    sort(V.begin(), V.end());
-    cin >> M;
-    FOR(0, M) {
-        int query_num;
-        cin >> query_num;
-        if (num_exists(query_num))
-            cout << 1 << endl;
-        else
+    sort(ALL(numVec), cmp);
+    REP(i, M) {
+        int query;
+        int bisectLeftIdx = lowerBound(numVec, query);
+        if (bisectLeftIdx < 0 || bisectLeftIdx >= N) {
             cout << 0 << endl;
+            continue;
+        }
+        cout << 1 << endl;
     }
     return 0;
 }
