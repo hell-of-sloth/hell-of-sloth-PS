d45d3a536c39dbeecac78af8a9ea125cb1e61ce1
Gyunseo Lee <rbstj0311@naver.com>
2024-07-29 21:59:05 +0900

    gyunseo: BOJ-9663

--- gyunseo/BOJ/9663/9663.cpp
+++ gyunseo/BOJ/9663/9663.cpp
@@ -1,63 +1,57 @@
-#include <algorithm>
 #include <cassert>
-#include <cstdlib>
 #include <iostream>
-#include <vector>
-#define endl '\n'
-#define ASSERT(exp, msg) assert(exp &msg)
+
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define ASSERT(exp, msg) assert(exp &&msg);
+#define REP(idx, end) for (auto idx = 0; idx < (end); ++idx)
+#define EACH(e, v) for (auto &e : (v))
+#define watch(x) cout << (#x) << ": " << x
+
 using namespace std;
 
-const int MAX = 15;
-int board[MAX + 1][MAX + 1];
-vector<int> is_issued_col;
 int N, ans;
+int board[14][14];
+bool checkCol[14], checkDiag1[28], checkDiag2[28];
 
-bool is_in_diagonal_path(int cur_col, int cur_row) {
-
-    int num_issued_row = is_issued_col.size();
-    for (int i = 0; i < num_issued_row; i++) {
-        int col = is_issued_col[i];
-        int row = i;
-        // int abs는 cstdlib에 있음
-        if (abs(cur_row - row) == abs(cur_col - col))
-            return true;
-    }
-    return false;
+bool isPromising(int nextLevel, int candidate) {
+    if (checkCol[candidate])
+        return false;
+    if (checkDiag1[nextLevel + candidate])
+        return false;
+    if (checkDiag2[nextLevel + N - 1 - candidate])
+        return false;
+    return true;
 }
 
-void dfs(int col, int row) {
-    if (row == N - 1) {
+void setCheckArrays(int nextLevel, int candidate, bool val) {
+    checkCol[candidate] = val;
+    checkDiag1[nextLevel + candidate] = val;
+    checkDiag2[nextLevel + N - 1 - candidate] = val;
+}
+void DFS(int curLevel, int promisingCol) {
+    if (curLevel == N - 1) {
         ans++;
         return;
     }
-    for (int i = 0; i < N; i++) {
-        if (find(is_issued_col.begin(), is_issued_col.end(), i) !=
-            is_issued_col.end())
-            continue;
-        if (is_in_diagonal_path(i, row + 1))
-            continue;
-        is_issued_col.push_back(i);
-        dfs(i, row + 1);
-        is_issued_col.pop_back();
+    
+    REP(cand, N) {
+        if (!isPromising(curLevel + 1, cand)) continue;
+        setCheckArrays(curLevel + 1, cand, true);
+        DFS(curLevel + 1, cand);
+        setCheckArrays(curLevel + 1, cand, false);
     }
 }
-
-void solve() {
-    for (int i = 0; i < N; i++) {
-        is_issued_col.push_back(i);
-        dfs(i, 0);
-        is_issued_col.pop_back();
-    }
-    cout << ans << endl;
-}
-
-void read_user_input() { cin >> N; }
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    read_user_input();
-    solve();
+    fastio;
+    cin >> N;
+    ASSERT((N >= 1 && N < 15), "N은 1이상 15미만");
+    REP(cand, N) {
+        if (!isPromising(0, cand)) continue;
+        setCheckArrays(0, cand, true);
+        DFS(0, cand);
+        setCheckArrays(0, cand, false);
+    }
+    cout << ans << endl;
     return 0;
 }
