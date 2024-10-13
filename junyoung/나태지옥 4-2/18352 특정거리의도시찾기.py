900ec0e68a533cf00316e7e406b4abf8f77587c8
SJY <jun000405@naver.com>
2024-10-13 23:56:16 +0900

    feat: junyoung 1013 다익

---
+++ junyoung/나태지옥 4-2/18352 특정거리의도시찾기.py
@@ -0,0 +1,44 @@
+# N = 300,000 / M = 1000000 MlogM ?
+
+import sys
+from heapq import heappop, heappush
+
+N, M, K, X = map(int, sys.stdin.readline().split())
+graph = {}
+
+for i in range(M):
+    a, b = map(int, sys.stdin.readline().split())
+    if graph.get(a) == None:
+        graph[a] = []
+    graph[a].append(b)
+    
+def djik(start):
+    hq = [(0, start)]
+    dist = [float('inf')] * (N+1)
+    dist[start] = 0
+    
+    while hq:
+        d, now_v = heappop(hq)
+        
+        if dist[now_v] != d:
+            continue
+        
+        if graph.get(now_v) == None:
+            continue
+        else:
+            for next_v in graph[now_v]:
+                if dist[next_v] > d + 1:
+                    dist[next_v] = d + 1
+                    heappush(hq, (d+1, next_v))
+                    
+    return dist
+
+result = djik(X)
+ans = -1
+for i in range(1, N+1):
+    if result[i] == K:
+        ans = i
+        sys.stdout.write(str(ans) + '\n')
+        
+if ans == -1:
+    print(-1)
